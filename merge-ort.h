8b07fd8b8c3558e5a13c2abdaa43a0e0908ffbbc
Elijah Newren <newren@gmail.com>
2017-12-18 17:35:10 -0800

    merge-ort: basic new merge strategy
    Copy functions from merge-recursive to implement a basic do-nothing
    merge strategy.  The eventual goal is to be a drop-in replacement for
    merge-recursive.c, but currently there is no actual merge logic beyond
    calling unpack_trees(), hoping it handles everything, and if it does,
    writing the merge out.
    
    Note that even the functions which follow the "End copy-paste static
    functions from merge-recursive.c" are still almost exact copy-paste,
    in order to allow me to rebase more easily.  The only differences are:
      * The logic in merge_trees_internal() for handling the unmerged
        entries has been gutted; this will be re-added later.
      * merge_trees() and merge_recursive() were renamed to avoid
        conflicting symbols so that the build can succeed.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>

---
+++ merge-ort.h
@@ -0,0 +1,25 @@
+#ifndef MERGE_ORT_H
+#define MERGE_ORT_H
+
+#include "merge-recursive.h"
+
+/*
+ * rename-detecting three-way merge, no recursion
+ * replacement for merge_trees()
+ */
+int merge_ort_nonrecursive(struct merge_options *opt,
+			   struct tree *head,
+			   struct tree *merge,
+			   struct tree *common);
+
+/*
+ * rename-detecting three-way merge with recursive ancestor consolidation
+ * replacement for merge_recursive()
+ */
+int merge_ort(struct merge_options *opt,
+	      struct commit *h1,
+	      struct commit *h2,
+	      struct commit_list *ancestors,
+	      struct commit **result);
+
+#endif
