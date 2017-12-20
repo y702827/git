929b7b28f5465b19831f7a3202bccb2b4246aee6
Elijah Newren <newren@gmail.com>
2017-12-20 14:12:39 -0800

    merge-ort: have unpack_trees() operate in index-only mode
    This will allow us to avoid making any changes to the working copy until
    the very end of the merge.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- merge-ort.c
+++ merge-ort.c
@@ -198,10 +198,7 @@ static int unpack_trees_start(struct merge_options *opt,
 	struct index_state tmp_index = { NULL };
 
 	memset(&opt->priv->unpack_opts, 0, sizeof(opt->priv->unpack_opts));
-	if (opt->priv->call_depth)
-		opt->priv->unpack_opts.index_only = 1;
-	else
-		opt->priv->unpack_opts.update = 1;
+	opt->priv->unpack_opts.index_only = 1;
 	opt->priv->unpack_opts.merge = 1;
 	opt->priv->unpack_opts.head_idx = 2;
 	opt->priv->unpack_opts.fn = threeway_merge;
