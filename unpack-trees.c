ffa6704f00ec659682f934c194b9e3edbd433ad1
Elijah Newren <newren@gmail.com>
2017-12-28 14:19:59 -0800

    unpack_trees: Make some functions available to merge strategies

--- unpack-trees.c
+++ unpack-trees.c
@@ -239,7 +239,7 @@ static int add_rejected_path(struct unpack_trees_options *o,
 /*
  * display all the error messages stored in a nice way
  */
-static void display_error_msgs(struct unpack_trees_options *o)
+void display_error_msgs(struct unpack_trees_options *o)
 {
 	int e;
 	unsigned error_displayed = 0;
@@ -1560,9 +1560,6 @@ static void populate_from_existing_patterns(struct unpack_trees_options *o,
 }
 
 
-static int verify_absent(const struct cache_entry *,
-			 enum unpack_trees_error_types,
-			 struct unpack_trees_options *);
 /*
  * N-way merge "len" trees.  Returns 0 on success, -1 on failure to manipulate the
  * resulting index, -2 on failure to reflect the changes to the work tree.
@@ -1830,7 +1827,7 @@ static int reject_merge(const struct cache_entry *ce,
 	return add_rejected_path(o, ERROR_WOULD_OVERWRITE, ce->name);
 }
 
-static int same(const struct cache_entry *a, const struct cache_entry *b)
+int cache_entries_same(const struct cache_entry *a, const struct cache_entry *b)
 {
 	if (!!a != !!b)
 		return 0;
@@ -1842,6 +1839,7 @@ static int same(const struct cache_entry *a, const struct cache_entry *b)
 	       oideq(&a->oid, &b->oid);
 }
 
+#define same(a,b) cache_entries_same(a,b)
 
 /*
  * When a CE gets turned into an unmerged entry, we
@@ -2131,9 +2129,9 @@ static int verify_absent_1(const struct cache_entry *ce,
 	}
 }
 
-static int verify_absent(const struct cache_entry *ce,
-			 enum unpack_trees_error_types error_type,
-			 struct unpack_trees_options *o)
+int verify_absent(const struct cache_entry *ce,
+		  enum unpack_trees_error_types error_type,
+		  struct unpack_trees_options *o)
 {
 	if (!o->skip_sparse_checkout && (ce->ce_flags & CE_NEW_SKIP_WORKTREE))
 		return 0;
--- unpack-trees.h
+++ unpack-trees.h
@@ -103,8 +103,14 @@ enum update_sparsity_result {
 
 enum update_sparsity_result update_sparsity(struct unpack_trees_options *options);
 
+void display_error_msgs(struct unpack_trees_options *o);
+int cache_entries_same(const struct cache_entry *a,
+		       const struct cache_entry *b);
 int verify_uptodate(const struct cache_entry *ce,
 		    struct unpack_trees_options *o);
+int verify_absent(const struct cache_entry *,
+		  enum unpack_trees_error_types,
+		  struct unpack_trees_options *);
 
 int threeway_merge(const struct cache_entry * const *stages,
 		   struct unpack_trees_options *o);
