f2a72fe46f29abe3c2133eb1f66092c8eeaa9989
Elijah Newren <newren@gmail.com>
2020-02-05 06:48:43 -0800

    t6046: improved conflict reporting (fixed a test!)

--- t/t6426-merge-skip-unneeded-updates.sh
+++ t/t6426-merge-skip-unneeded-updates.sh
@@ -666,7 +666,7 @@ test_setup_4a () {
 #   correct requires doing the merge in-memory first, then realizing that no
 #   updates to the file are necessary, and thus that we can just leave the path
 #   alone.
-test_expect_failure '4a: Change on A, change on B subset of A, dirty mods present' '
+test_expect_success '4a: Change on A, change on B subset of A, dirty mods present' '
 	test_setup_4a &&
 	(
 		cd 4a &&
