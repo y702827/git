349e5994b51bfebd8c5f8d722b5d1fb8bbe63d34
Elijah Newren <newren@gmail.com>
2020-02-26 19:46:55 -0800

    I guess we need string_list_df_name_compare after all
    Now passes the new t6020 testcase.

--- merge-ort.c
+++ merge-ort.c
@@ -268,6 +268,33 @@ static struct commit_list *reverse_commit_list(struct commit_list *list)
 	return next;
 }
 
+static int string_list_df_name_compare(const char *one, const char *two)
+{
+	int onelen = strlen(one);
+	int twolen = strlen(two);
+	/*
+	 * Here we only care that entries for D/F conflicts are adjacent,
+	 * in particular with the file of the D/F conflict appearing before
+	 * files below the corresponding directory.  The order of the rest
+	 * of the list is irrelevant for us.
+	 *
+	 * To achieve this, we sort with df_name_compare and provide the
+	 * mode S_IFDIR so that D/F conflicts will sort correctly.  We use
+	 * the mode S_IFDIR for everything else for simplicity, since in
+	 * other cases any changes in their order due to sorting cause no
+	 * problems for us.
+	 */
+	int cmp = df_name_compare(one, onelen, S_IFDIR,
+				  two, twolen, S_IFDIR);
+	/*
+	 * Now that 'foo' and 'foo/bar' compare equal, we have to make sure
+	 * that 'foo' comes before 'foo/bar'.
+	 */
+	if (cmp)
+		return cmp;
+	return onelen - twolen;
+}
+
 /***** End copy-paste static functions from merge-recursive.c *****/
 
 static void setup_path_info(struct string_list_item *result,
@@ -2763,11 +2790,7 @@ static void process_entries(struct merge_options *opt,
 	strmap_for_each_entry(&opt->priv->paths, &iter, e) {
 		string_list_append(&plist, e->item.string)->util = e->item.util;
 	}
-	/*
-	 * Note: although standard strcmp-ordered sort does not put D/F
-	 * entries near each other, that doesn't matter for our needs.
-	 * If it did, we could: plist.cmp = string_list_df_name_compare;
-	 */
+	plist.cmp = string_list_df_name_compare;
 	string_list_sort(&plist);
 
 	/*
