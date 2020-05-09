a97c435f658ff70469abf02fbf240772e11277ed
Elijah Newren <newren@gmail.com>
2020-05-09 11:56:01 -0700

    diffcore-rename: simplify and accelerate register_rename_src()
    reigster_rename_src() took pains to create an array in rename_src which
    was sorted by pathname of the contained diff_filepair.  However, the
    fact that this array was sorted was not needed anywhere, and thus
    represented wasted time.  Simply append to the end of the array, to save
    45% of diffcore_rename_extended() setup time, and 2% of overall
    execution time of rebasing a series of commits with lots of renames.
    
    Technically, there is one difference in the end result for the code.  IF
    the caller of diffcore-rename provides a set of pairs with a duplicate
    filename in the sources (an erroneous input condition), the old code
    would simply ignore the duplicate (without warning or error).  The new
    code will simply swallow it and thus allow multiple pairings for the
    "same" source file.  Checking for this error condition is expensive
    (basically undoing the optimization) and the only callers are the
    diffcore and merge-ort machinery which are already programmed to
    correctly avoid passing invalid inputs.  If someone wants to add such
    error checking, I believe they should add a separate function which
    callers can call before calling diffcore_rename[_extended]().
    
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- diffcore-rename.c
+++ diffcore-rename.c
@@ -84,36 +84,12 @@ static struct diff_rename_src {
 } *rename_src;
 static int rename_src_nr, rename_src_alloc;
 
-static struct diff_rename_src *register_rename_src(struct diff_filepair *p)
+static void register_rename_src(struct diff_filepair *p)
 {
-	int first, last;
-	struct diff_filespec *one = p->one;
-	unsigned short score = p->score;
-
-	first = 0;
-	last = rename_src_nr;
-	while (last > first) {
-		int next = first + ((last - first) >> 1);
-		struct diff_rename_src *src = &(rename_src[next]);
-		int cmp = strcmp(one->path, src->p->one->path);
-		if (!cmp)
-			return src;
-		if (cmp < 0) {
-			last = next;
-			continue;
-		}
-		first = next+1;
-	}
-
-	/* insert to make it at "first" */
 	ALLOC_GROW(rename_src, rename_src_nr + 1, rename_src_alloc);
+	rename_src[rename_src_nr].p = p;
+	rename_src[rename_src_nr].score = p->score;
 	rename_src_nr++;
-	if (first < rename_src_nr)
-		MOVE_ARRAY(rename_src + first + 1, rename_src + first,
-			   rename_src_nr - first - 1);
-	rename_src[first].p = p;
-	rename_src[first].score = score;
-	return &(rename_src[first]);
 }
 
 static int basename_same(struct diff_filespec *src, struct diff_filespec *dst)
