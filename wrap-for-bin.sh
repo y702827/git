f16879e7e4ad475ec4f2b6fcd15fc62be014fa3b
Elijah Newren <newren@gmail.com>
2020-08-15 23:10:04 -0700

    Tweaks to bin-wrappers for running valgrind selectively
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- wrap-for-bin.sh
+++ wrap-for-bin.sh
@@ -20,6 +20,14 @@ PATH='@@BUILD_DIR@@/bin-wrappers:'"$PATH"
 
 export GIT_EXEC_PATH GITPERLLIB PATH GIT_TEXTDOMAINDIR
 
+VALGRIND_RESULTS="valgrind-results.txt"
+if [[ "$1" == "rev-list" || "$1" == "log" ]]; then
+    if [[ -z "$GIT_DEBUGGER" ]]; then
+	rm -f $VALGRIND_RESULTS
+	GIT_DEBUGGER="valgrind --track-origins=yes --leak-check=yes --log-file=$VALGRIND_RESULTS"
+    fi
+fi
+
 case "$GIT_DEBUGGER" in
 '')
 	exec "${GIT_EXEC_PATH}/@@PROG@@" "$@"
@@ -31,6 +39,15 @@ case "$GIT_DEBUGGER" in
 *)
 	GIT_DEBUGGER_ARGS="$GIT_DEBUGGER"
 	unset GIT_DEBUGGER
-	exec ${GIT_DEBUGGER_ARGS} "${GIT_EXEC_PATH}/@@PROG@@" "$@"
+	${GIT_DEBUGGER_ARGS} "${GIT_EXEC_PATH}/@@PROG@@" "$@"
+	ret=$?
+	if test $? -eq 0; then
+	    grep -q "ERROR SUMMARY: 0 errors from 0 contexts" $VALGRIND_RESULTS
+	    ret=$?
+	    if test $ret -ne 0; then
+		cat $VALGRIND_RESULTS >&2
+	    fi
+	fi
+	exit $ret
 	;;
 esac
